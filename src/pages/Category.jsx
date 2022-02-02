6020787f816241db557255376eed5480dddbd65e
gchis66 <gchis66@gmail.com>
2022-02-02 13:57:11 -0500

    completed house marketplace project

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -4,9 +4,10 @@ import {collection, getDocs, query, where, orderBy, limit, startAfter} from 'fir
 import {db} from '../firebase.config'
 import {toast} from 'react-toastify'
 import Spinner from '../components/Spinner'
+import ListingItem from '../components/ListingItem' 
 
 function Category() {
-    const [listings, setListings] = useState(null)
+    const [listings, setListings] = useState(null)   
     const [loading, setLoading] = useState(true)
 
     const params = useParams()
@@ -53,7 +54,7 @@ function Category() {
                 <main>
                     <ul className="categoryListings">
                         {listings.map((listing) => (
-                            <h3>{listing.data.name}</h3>
+                            <ListingItem listing={listing.data} id={listing.id} key={listing.id}/>
                         ))}
                     </ul>
                 </main>
